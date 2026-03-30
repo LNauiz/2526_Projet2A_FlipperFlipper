53e837ebbebc288b454a7d2307fbb9dd46b61e80
Léonard Navizet <leonard.navizet@ensea.fr>
2026-03-30 18:57:18 +0200

    epuration

--- Soft/PCB_Final/Core/Src/utils.c
+++ Soft/PCB_Final/Core/Src/utils.c
@@ -5,7 +5,8 @@
 
 
 void setup(){
-
+	println("Hello Worlds \n\r");
+	HAL_GPIO_WritePin(LED_Status_GIO_Port,LED_Status_Pin,1);
 }
 //LED_STATUS_GPIO_Port
 
@@ -13,10 +14,15 @@ void loop(){
 	if (HAL_GPIO_ReadPin(Bouton_Droit_GPIO_Port,Bouton_Droit_Pin)){
 		HAL_GPIO_WritePin(Com_Sol_A_GPIO_Port,Com_Sol_A_Pin,1);
 	}
+	else{
+		HAL_GPIO_WritePin(Com_Sol_A_GPIO_Port,Com_Sol_A_Pin,0);
+	};
 	if (HAL_GPIO_ReadPin(Bouton_Gauche_GPIO_Port,Bouton_Gauche_Pin)){
 		HAL_GPIO_WritePin(Com_Sol_B_GPIO_Port,Com_Sol_B_Pin,1);
 	}
-
+	else{
+		HAL_GPIO_WritePin(Com_Sol_B_GPIO_Port,Com_Sol_B_Pin,0);
+	}
 };
 
 
